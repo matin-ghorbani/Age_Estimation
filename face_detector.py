52b56a6e392d6a9f78e4cdb826412a12190508b6
Matin Ghorbani <porya.ghorbani10@gmail.com>
2024-04-26 07:35:55 +0330

    Processing on video or webcam

--- face_detector.py
+++ face_detector.py
@@ -1,11 +1,14 @@
 import cv2 as cv
+import tensorflow as tf
+from keras.models import Sequential, load_model
 
 
 class FaceDetector:
-    def __init__(self) -> None:
+    def __init__(self, age_estimator_model: str | None = None) -> None:
         self.face_detector = cv.CascadeClassifier(
             cv.data.haarcascades + "haarcascade_frontalface_alt.xml"
         )
+        self.age_estimator: Sequential | None = load_model(age_estimator_model) if age_estimator_model else None
 
     def detect_face(self, img: cv.typing.MatLike, draw: bool = True) -> list[
         cv.typing.MatLike, list[list[int, int, int, int]]]:
@@ -18,13 +21,23 @@ class FaceDetector:
         for face in faces:
             x, y, w, h = face
             x, w = x - 20, w + 20
-            y, h = y - 20, h + 40
+            y, h = y - 20, h + 50
 
             bboxs.append((x, y, w + x, h + y))
             if draw:
                 result_img = FaceDetector.draw(img.copy(), (x, y, w + x, h + y))
 
         return result_img, bboxs
+
+    def estimate_age(self, img: cv.typing.MatLike):
+        img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        img = cv.resize(img, (124, 124))
+        img = img / 255.
+
+        img = img[tf.newaxis, ...]
+
+        prediction = self.age_estimator.predict(img)
+        return round(float(prediction[0][0]), 2)
 
     @staticmethod
     def draw(img, bbox) -> cv.typing.MatLike:
--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import cv2 as cv
 from face_detector import FaceDetector
 
-detector = FaceDetector()
+detector = FaceDetector('./weights/best_age_estimator_resnet.h5')
 
 cap = cv.VideoCapture(0)
 while True:
     frame = cap.read()[1]
-    frame, bboxs = detector.detect_face(frame, draw=True)
+    result_frame, bboxs = detector.detect_face(frame, draw=True)
 
-    cv.imshow('webcam', frame)
+    if len(bboxs) > 0:
+        for bbox in bboxs:
+            x1, y1, x2, y2 = bbox
+            face = frame[y1: y2, x1: x2]
+            age = detector.estimate_age(face)
+            cv.putText(result_frame, f'Age: {age}', (x1, y2 + 20),
+                       cv.FONT_HERSHEY_PLAIN, 1.0, (255, 255, 255), 2)
+
+    cv.imshow('webcam', result_frame)
     if cv.waitKey(1) & 0xFF == 27:
         break
 
